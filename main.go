2b141446c3b0d4d0b9f3186de185508cbc09642e
niwahc.o <windzie@gmail.com>
2018-11-30 14:57:44 +0700

    init startServer

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"os"
-
+	"bankproject/bankapi"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	createTable := `
-	CREATE TABLE IF NOT EXISTS todos (
-		id SERIAL PRIMARY KEY,
-		todo TEXT,
-		created_at TIMESTAMP WITHOUT TIME ZONE,
-		updated_at TIMESTAMP WITHOUT TIME ZONE
-	);
-	CREATE TABLE IF NOT EXISTS secrets (
-		id SERIAL PRIMARY KEY,
-		key TEXT
-	);
-	`
-	if _, err := db.Exec(createTable); err != nil {
-		log.Fatal(err)
-	}
+	bankapi.StartServer()
 }
