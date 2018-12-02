d3d9f4afe2a3294d631f5c08b14887718c98f4f1
niwahc.o <windzie@gmail.com>
2018-12-02 20:39:58 +0700

    change amount to balance db field name

--- bankapi/api.go
+++ bankapi/api.go
@@ -312,7 +312,7 @@ func (s *Server) DepositByID(c *gin.Context) {
 }
 
 func (s *BankAccountServiceImp) Deposit(id int, amount int) (*BankAccount, error) {
-	stmt := "SELECT id, user_id, amount FROM bank_accounts WHERE id = $1"
+	stmt := "SELECT id, user_id, balance FROM bank_accounts WHERE id = $1"
 	row := s.db.QueryRow(stmt, id)
 	var bankAccount BankAccount
 	err := row.Scan(&bankAccount.ID, &bankAccount.UserID, &bankAccount.Balance)
@@ -360,7 +360,7 @@ func (s *Server) WithdrawByID(c *gin.Context) {
 }
 
 func (s *BankAccountServiceImp) Withdraw(id int, amount int) (*BankAccount, error) {
-	stmt := "SELECT id, user_id, amount FROM bank_accounts WHERE id = $1"
+	stmt := "SELECT id, user_id, balance FROM bank_accounts WHERE id = $1"
 	row := s.db.QueryRow(stmt, id)
 	var bankAccount BankAccount
 	err := row.Scan(&bankAccount.ID, &bankAccount.UserID, &bankAccount.Balance)
